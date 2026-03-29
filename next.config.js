f290b8c422d3812fbc70b48132556c14aa6d35a0
Taxgab <Taxgab@gmail.com>
2026-03-29 02:35:56 -0300

    fix: agregar redirección de asistencia.html a /asistencia

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/asistencia.html',
+        destination: '/asistencia',
+        permanent: true,
+      },
+    ]
+  },
+}
 
 module.exports = nextConfig
